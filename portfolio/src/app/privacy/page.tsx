760097ddbea4faee52962765efe64a6698b0ba82
Dylan Morgan <114700444+vaeleborne@users.noreply.github.com>
2025-06-05 21:45:20 -0700

    Fix?

--- portfolio/src/app/privacy/page.tsx
+++ portfolio/src/app/privacy/page.tsx
@@ -1,5 +1,5 @@
-import Introduction from "./components/privacy/Privacy";
-import "../../styles/app.css";
+import Privacy from "../components/privacy/Privacy";
+import "../styles/app.css";
 
 export const metadata = {
   title: "Privacy",
